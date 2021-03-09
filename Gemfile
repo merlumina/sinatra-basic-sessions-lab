fad1c3f31347b287d964df5e5658eb50089edf66
Liz <lizriderwilson@gmail.com>
2021-03-09 11:11:11 -0500

    commit tree 0d28fd03f548dc019196e269c9774f30519faee1

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 source "https://rubygems.org"
 
 # gem "rails"
+gem 'securerandom'
 
 gem 'sinatra'
     gem 'rake'
--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
+require 'securerandom'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
+
 end
